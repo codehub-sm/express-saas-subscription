1a979b592eed5f8b3003c04d8086169d5399513c
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 01:13:34 +0530

    Added EpicTax module

---
+++ EpicPaymentService/EpicTax/index.js
@@ -0,0 +1,9 @@
+// This is the EpicTax module which will interface with Avalara for tax calculations.
+
+// Placeholder for tax calculation logic
+function calculateTax() {
+    // Todo: Implement tax calculation logic here
+}
+
+// Export the tax calculation logic
+aficTax.calculateTax = calculateTax;
