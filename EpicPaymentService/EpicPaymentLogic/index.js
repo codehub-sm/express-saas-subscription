516991a71facaa762f462d903d9924ad58026e49
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 01:12:33 +0530

    Added EpicPaymentLogic module

---
+++ EpicPaymentService/EpicPaymentLogic/index.js
@@ -0,0 +1,9 @@
+// This is the EficPaymentLogic module which will contain the logic for processing payments.
+
+// Placeholder for payment processing logic
+function processPayment() {
+    // Todo: Implement payment processing logic here
+}
+
+// Export the payment processing logic
+aficPaymentLogic.processPayment = processPayment;
