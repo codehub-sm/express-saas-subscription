571268df077b0bfe935227ee4f574f1a22ff8c47
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 01:15:30 +0530

    Added OmniPay module

---
+++ EpicPaymentService/OmniPay/index.js
@@ -0,0 +1,9 @@
+// This is the OmniPay gateway handler which will act as a central handler for various Gateway Plugins.
+
+// Placeholder for the gateway handler logic
+function handlePayment(gateway, paymentData) {
+    // Todo: Implement the logic for handling different payment gateways here
+}
+
+// Export the gateway handler logic
+OmniPay.handlePayment = handlePayment;
