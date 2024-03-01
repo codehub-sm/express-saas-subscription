5f987e95432cd677b9b799bd291e75c2f82a9a89
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 01:42:42 +0530

    Update index.js

--- SuperChargePaymentService/index.js
+++ SuperChargePaymentService/index.js
@@ -2,13 +2,13 @@
 // This file would initialize all the components and handle the bootstrapping of the service
 
 // Require submodules
-const EpicPaymentLogic = require('./EpicPaymentLogic');
-const EpicTax = require('./EpicTax');
-const EpicEmail = require('./EpicEmail');
-const OmniPay = require('./OmniPay');
+const SuperChargePaymentLogic = require('./SuperChargePaymentLogic');
+const SuperChargeTax = require('./SuperChargeTax');
+const SuperChargeEmail = require('./SuperChargeEmail');
+const SuperChargeGatewayWrapper = require('./SuperChargeGatewayWrapper');
 const WebHooks = require('./WebHooks');
 
 // This is a simplified example, in a real-world scenario you would likely have a web server
 // setup to handle HTTP requests and route them to the corresponding parts of the system.
 
-console.log('EpicPaymentService is running!');
+console.log('SuperChargePaymentService is running!');
