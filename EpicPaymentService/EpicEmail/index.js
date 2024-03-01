0c2a2256d98b7fd1fa8a823591d5c50a16b41ed5
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 01:14:16 +0530

    Added EpicEmail module

---
+++ EpicPaymentService/EpicEmail/index.js
@@ -0,0 +1,9 @@
+// This is the EpicEmail module which will interface with email services like Iterable.
+
+// Placeholder for email sending logic
+function sendEmail() {
+    // Todo: Implement email sending logic here
+}
+
+// Export the email sending logic
+ExpicEmail.sendEmail = sendEmail;
