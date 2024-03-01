56727734ea869e9d8947251f3508b771b63047bd
codehub-sm <78898056+codehub-sm@users.noreply.github.com>
2024-03-02 00:57:44 +0530

    Initial setup for Express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`)});
